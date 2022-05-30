64e772a2c5ea6d9c1adf93885e6cfdb570aa5cd9
abhinavVuthoori <vuthooriabhi02@gmail.com>
2022-05-30 12:45:15 +0530

    added login

---
+++ src/components/Login.js
@@ -0,0 +1,20 @@
+function Login() {
+    return ( 
+        <div className="container w-50">
+            <form action="">
+            <p className="display-3 text-warning mb-2">Login</p>
+            <div className="mb-3">
+                <label htmlFor="un">Username</label>
+                <input type="text" className="form-control" id="un" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="ps">Password</label>
+                    <input type="password"  id="ps" className="form-control"/>
+                </div>
+                <button className="btn-info">Signin</button>
+            </form>
+        </div>
+     );
+}
+
+export default Login;
