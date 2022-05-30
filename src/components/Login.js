378fe7921ac3e101be030aa10ccea5b7cd21909b
abhinavVuthoori <vuthooriabhi02@gmail.com>
2022-05-30 13:05:44 +0530

    got login data

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,26 @@
+import { useForm } from "react-hook-form";
+
+
 function Login() {
+
+    const {register,handleSubmit,formState:{errors}}=useForm()
+
+    const onFormSubmit=(obj)=>{
+        console.log(obj)
+    }
     return ( 
         <div className="container w-50">
-            <form action="">
+            <form onSubmit={handleSubmit(onFormSubmit)}>
             <p className="display-3 text-warning mb-2">Login</p>
             <div className="mb-3">
                 <label htmlFor="un">Username</label>
-                <input type="text" className="form-control" id="un" />
+                <input type="text" className="form-control" id="un" {...register("username")}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="ps">Password</label>
-                    <input type="password"  id="ps" className="form-control"/>
+                    <input type="password"  id="ps" className="form-control" {...register("password")}/>
                 </div>
-                <button className="btn-info">Signin</button>
+                <button className="btn-info" type="submit">Signin</button>
             </form>
         </div>
      );
