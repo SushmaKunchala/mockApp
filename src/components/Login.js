3c198df56639aa15ec1b78a5c98a2de7d557376f
shreshtan19 <shreshtanadipelli19@gmail.com>
2022-05-30 14:52:48 +0530

    login commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import { Nav, NavItem, NavLink } from "reactstrap";
 
 
 function Login() {
@@ -9,20 +10,51 @@ function Login() {
         console.log(obj)
     }
     return ( 
-        <div className="container w-50">
-            <form onSubmit={handleSubmit(onFormSubmit)}>
-            <p className="display-3 text-warning mb-2">Login</p>
-            <div className="mb-3">
-                <label htmlFor="un">Username</label>
-                <input type="text" className="form-control" id="un" {...register("username")}/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="ps">Password</label>
-                    <input type="password"  id="ps" className="form-control" {...register("password")}/>
-                </div>
-                <button className="btn-info" type="submit">Signin</button>
-            </form>
-        </div>
+
+        <div className="form"> 
+
+     <Nav pills>
+            <NavItem>
+            <NavLink href="#">teacher</NavLink>
+        <form onSubmit={handleSubmit(onFormSubmit)}>
+
+   <div className="mb-3">
+    <label htmlFor="un">Username</label>
+    <input type="text" className="form-control" id="un" {...register("username")}/>
+   </div>
+
+    <div className="mb-3">
+        <label htmlFor="ps">Password</label>
+        <input type="password"  id="ps" className="form-control" {...register("password")}/>
+    </div>
+    <button className="btn-info" type="submit">Signin</button>    
+   </form>
+            </NavItem>
+
+                <NavItem>
+                <NavLink href="#">employee</NavLink>
+                    
+     <form onSubmit={handleSubmit(onFormSubmit)}>
+
+   <div className="mb-3">
+    <label htmlFor="un">Username</label>
+    <input type="text" className="form-control" id="un" {...register("username")}/>
+   </div>
+
+    <div className="mb-3">
+        <label htmlFor="ps">Password</label>
+        <input type="password"  id="ps" className="form-control" {...register("password")}/>
+    </div>
+
+    <button className="btn-info" type="submit">Signin</button>
+
+     </form>
+     
+    </NavItem>
+    </Nav>
+
+           </div>
+           
      );
 }
 
